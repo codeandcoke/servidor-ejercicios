ad16f2aeab117ca529331a0136943cce5913641f
profesor <profesor@debian>
2017-10-25 18:51:58 +0200

    [AgenciaViajes] Terminado el boceto de la portada

---
+++ AgenciaViajes/config/configuracion.php
@@ -0,0 +1,5 @@
+<?php
+define("DB_USUARIO", "viajes");
+define("DB_HOST", "localhost");
+define("DB_NOMBRE", "viajes");
+define("DB_PASSWORD", "viajes");
---
+++ AgenciaViajes/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="icons/favicon.ico">
+
+    <title>Viajes Montessori</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">VM</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Viajes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Contacto</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Proveedores</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-3">Viajes Montessori</h1>
+        <p>Agencia de viajes especializada en Programadores</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver todos los viajes &raquo;</a></p>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <?php
+        include("config/configuracion.php");
+
+
+        ?>
+        <div class="card" style="width: 20rem;">
+          <img class="card-img-top" src="img/google.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Viaja a la sede de Google</h4>
+            <p class="card-text">Viaje a Silicon Valley</p>
+            <a href="#" class="btn btn-primary">Leer más</a>
+          </div>
+        </div>
+        <div class="card" style="width: 20rem;">
+          <img class="card-img-top" src="img/microsoft.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Viaja a Microsoft</h4>
+            <p class="card-text">Viaja a la sede del Sistema Operativo Windows</p>
+            <a href="#" class="btn btn-primary">Leer más</a>
+          </div>
+        </div>
+        <div class="card" style="width: 20rem;">
+          <img class="card-img-top" src="img/marenostrum.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Viaje al Marenostrum</h4>
+            <p class="card-text">Visita el supercomputador más potente de Europa</p>
+            <a href="#" class="btn btn-primary">Leer más</a>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; Viajes Montessori 2017</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/vendor/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
