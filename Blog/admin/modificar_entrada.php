d493cdedef02dccbcd698762d9da14d9c907e431
profesor <profesor@debian>
2017-12-14 21:29:57 +0100

    [Blog] Añadida la opción de modificar el titulo y texto de las entradas del blog

---
+++ Blog/admin/modificar_entrada.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if (!isset($_SESSION["usuario"])) {
+    header("Location: login.php");
+}
+include("../config/db.php");
+include("../include/funciones.php");
+
+$id = $_POST["id"];
+$titulo = $_POST["titulo"];
+$texto = $_POST["texto"];
+
+
+$db = new Db();
+$sql = "UPDATE entradas SET titulo = ?, texto = ? WHERE id = ?";
+$db->lanzar_consulta($sql, array($titulo, $texto, $id));
+$db->desconectar();
+header("Location: index.php?id=entradas");
--- Blog/admin/nueva_entrada.php
+++ Blog/admin/nueva_entrada.php
@@ -1,15 +1,48 @@
+<?php
+if (isset($_REQUEST["entrada"])) {
+    $entrada = $_REQUEST["entrada"];
+
+    $sql = "SELECT * FROM entradas WHERE id = ?";
+    $db = new Db();
+    $resultado = $db->lanzar_consulta($sql, array($entrada));
+    $fila = $resultado->fetch_assoc();
+}
+?>
 <h2>Nueva Entrada</h2>
+<?php
+if (isset($entrada)) {
+?>
+<form action="modificar_entrada.php" method="post">
+<?php
+}
+else {
+?>
 <form action="alta_entrada.php" method="post">
+<?php
+}
+?>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="titulo">Titulo</label>
-            <input type="text" name="titulo" class="form-control" id="titulo" placeholder="Título">
+            <input type="text" name="titulo" class="form-control"
+                   id="titulo" placeholder="Título"
+                <?php
+                if (isset($entrada)) {
+                    echo "value='" . $fila["titulo"] . "'";
+                }
+                ?>>
         </div>
     </div>
     <div class="form-row">
         <div class="form-group col-md-12">
             <label for="texto">Descripcion</label>
-            <textarea name="texto" cols="20" rows="10" class="form-control" id="texto" placeholder="Texto"></textarea>
+            <textarea name="texto" cols="20" rows="10" class="form-control" id="texto" placeholder="Texto">
+                <?php
+                if (isset($entrada)) {
+                    echo $fila["texto"];
+                }
+                ?>
+            </textarea>
         </div>
     </div>
     <div class="form-row">
@@ -17,6 +50,11 @@
             <input type="text" name="etiquetas" size="50" placeholder="Etiquetas (separadas por coma)" class="form-control"/>
         </div>
     </div>
+    <?php
+    if (isset($entrada)) {
+        echo "<input type='hidden' name='id' value='" . $fila["id"] . "'>";
+    }
+    ?>
     <div class="form-row">
         <button class="btn btn-primary" type="submit">Enviar</button>
     </div>
